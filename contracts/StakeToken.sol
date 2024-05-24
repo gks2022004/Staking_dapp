3f7c3c6d9f4562bd2b4f81d1c46fa0c32a5555fb
gks2022004 <imgauravkumar7@gmail.com>
2024-05-24 12:53:37 +0530

    created staking algo.

---
+++ contracts/StakeToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract StakeToken is ERC20{
+    constructor(uint256 initialSupply) ERC20("StakeToken", "SWT"){
+        _mint(msg.sender,initialSupply*10**18);
+    }
+
+    function decimals() public pure override returns(uint8){
+        return 0;
+    }
+}
